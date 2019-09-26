3ad27324050ecf9760bf30a5a5a71b3b30d642cf
Bobby Earl <bobby.earl@blackbaud.com>
2019-09-26 14:52:14 -0400

    Added check and warning/error if certificate not found.

--- cli/utils/cert-resolver.js
+++ cli/utils/cert-resolver.js
@@ -1,18 +1,35 @@
 /*jslint node: true */
 'use strict';
 
+const fs = require('fs-extra');
 const os = require('os');
 const path = require('path');
+const logger = require('@blackbaud/skyux-logger');
 
-// PATH SHARED WITH CLI
-const resolver = require(path.resolve(`${os.homedir()}/.skyux/certs/`));
+function getResolver() {
+  const certRootPath = path.resolve(`${os.homedir()}/.skyux/certs/`);
+
+  if (!fs.pathExistsSync(certRootPath)) {
+    logger.error('Unable to locate certificates.');
+    logger.error('Please install the latest SKY UX CLI and run `skyux certs install`.');
+  } else {
+    const resolver = require(certRootPath);
+    return resolver;
+  }
+}
 
 function readCert() {
-  return resolver.readCert();
+  const resolver = getResolver();
+  if (resolver) {
+    return resolver.readCert();
+  }
 }
 
 function readKey() {
-  return resolver.readKey();
+  const resolver = getResolver();
+  if (resolver) {
+    return resolver.readKey();
+  }
 }
 
 module.exports = {
