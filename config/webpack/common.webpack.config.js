19396e6dbdc0c0ffc4da307d4e93a36ca5d5016c
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-01-30 10:13:19 -0500

    Comment cleanup

--- config/webpack/common.webpack.config.js
+++ config/webpack/common.webpack.config.js
@@ -71,7 +71,7 @@ function getWebpackConfig(skyPagesConfig, argv = {}) {
       skyux: skyPagesConfig.skyux
     }),
 
-    // The `ngDevMode` property is usually defined by Angular CLI during a production build.
+    // The `ngDevMode` property is defined by Angular CLI during a production build.
     // We need to set it to 'false' to get the build to pass.
     // See: https://github.com/angular/angular/issues/31595#issuecomment-519083266
     new webpack.DefinePlugin({
