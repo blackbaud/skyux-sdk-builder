a570a088d97c379570e00e5e58a6e6bc2a507425
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-02-05 15:12:20 -0500

    Code cleanup

--- cli/build-public-library.js
+++ cli/build-public-library.js
@@ -4,84 +4,118 @@
 const fs = require('fs-extra');
 const ngPackage = require('ng-packagr');
 const rimraf = require('rimraf');
+const logger = require('@blackbaud/skyux-logger');
 
 const stageTypeScriptFiles = require('./utils/stage-library-ts');
 const preparePackage = require('./utils/prepare-library-package');
 const skyPagesConfigUtil = require('../config/sky-pages/sky-pages.config');
+const tsLinter = require('./utils/ts-linter');
 
-const DIST_DIR = skyPagesConfigUtil.spaPath('dist');
+function runLinter(argv) {
+  const lintResult = tsLinter.lintSync(argv);
+  if (lintResult.exitCode > 0) {
+    process.exit(lintResult.exitCode);
+  }
+}
 
-const cleanDist = () => rimraf.sync(DIST_DIR);
-const cleanTemp = () => rimraf.sync(skyPagesConfigUtil.spaPathTemp());
+function cleanTemp() {
+  rimraf.sync(skyPagesConfigUtil.spaPathTemp());
+}
+
+function cleanDist() {
+  rimraf.sync(skyPagesConfigUtil.spaPath('dist'));
+}
 
 function cleanAll() {
   cleanTemp();
   cleanDist();
 }
 
-function writeTsConfig() {
-  const defaultConfigPath = skyPagesConfigUtil.spaPath(
-    'node_modules/ng-packagr/lib/ts/conf/tsconfig.ngc.json'
+function copyDist() {
+  fs.copySync(
+    skyPagesConfigUtil.spaPathTemp('dist'),
+    skyPagesConfigUtil.spaPath('dist')
   );
 
-  const tsConfig = {
-    extends: defaultConfigPath,
-    compilerOptions: {}
-  };
+}
 
-  fs.writeJsonSync(
-    skyPagesConfigUtil.spaPathTemp('tsconfig.lib.json'),
-    tsConfig
+function copyRuntime() {
+  fs.copySync(
+    skyPagesConfigUtil.outPath('runtime'),
+    skyPagesConfigUtil.spaPathTemp('runtime')
   );
 }
 
-function createNgPackageJson() {
+function cleanRuntime() {
+  rimraf.sync(skyPagesConfigUtil.spaPath('dist', 'runtime'));
+}
+
+function writeTSConfig() {
+  const config = {
+    extends: skyPagesConfigUtil.spaPath(
+      'node_modules/ng-packagr/lib/ts/conf/tsconfig.ngc.json'
+    ),
+    angularCompilerOptions: {
+      fullTemplateTypeCheck: false,
+    }
+  };
+
+  fs.writeJSONSync(skyPagesConfigUtil.spaPathTemp('tsconfig.json'), config);
+}
+
+function processFiles(skyPagesConfig) {
+  const pluginFileProcessor = require('../lib/plugin-file-processor');
+  pluginFileProcessor.processFiles(
+    skyPagesConfig,
+    skyPagesConfigUtil.spaPathTemp('**', '*.*')
+  );
+}
+
+/**
+ * Transpiles TypeScript files into JavaScript files
+ * to be included with the NPM package.
+ */
+function transpile() {
+  const projectConfigPath = skyPagesConfigUtil.spaPathTemp('ng-package.json');
+  const tsConfigPath = skyPagesConfigUtil.spaPathTemp('tsconfig.json');
+
+  return ngPackage.ngPackagr()
+    .forProject(projectConfigPath)
+    .withTsConfig(tsConfigPath)
+    .build();
+}
+
+function writePackagerConfig() {
   const ngPackageConfig = {
     lib: {
       entryFile: 'index.ts'
     }
   };
 
-  fs.writeJsonSync(
-    skyPagesConfigUtil.spaPathTemp('ng-package.json'),
-    ngPackageConfig
-  );
+  fs.writeJsonSync(skyPagesConfigUtil.spaPathTemp('ng-package.json'), ngPackageConfig);
 }
 
-function runPackager() {
-  const projectConfigPath = skyPagesConfigUtil.spaPathTemp('ng-package.json');
-  const tsConfigPath = skyPagesConfigUtil.spaPathTemp('tsconfig.lib.json');
-
-  return ngPackage
-    .ngPackagr()
-    .forProject(projectConfigPath)
-    .withTsConfig(tsConfigPath)
-    .build();
-}
-
-function finalPolish() {
-  preparePackage();
-
-  fs.copySync(
-    skyPagesConfigUtil.spaPathTemp('dist'),
-    DIST_DIR
-  );
-}
-
-async function buildPublicLibrary() {
+module.exports = (argv, skyPagesConfig, webpack) => {
+  runLinter(argv);
   cleanAll();
   stageTypeScriptFiles();
-  writeTsConfig();
-  createNgPackageJson();
+  writeTSConfig();
+  writePackagerConfig();
+  copyRuntime();
+  processFiles(skyPagesConfig);
 
-  try {
-    await runPackager();
-    finalPolish();
-    console.log('Package success.');
-  } catch (error) {
-    console.log('Package error:', error);
-    process.exit(1);
-  }
-}
-
-module.exports = buildPublicLibrary;
+  return transpile()
+    .then(() => {
+      cleanRuntime();
+      copyDist();
+      preparePackage();
+      cleanTemp();
+      logger.info('SKY UX library built successfully.');
+      process.exit(0);
+    })
+    .catch((err) => {
+      cleanAll();
+      logger.error(err);
+      process.exit(1);
+    });
+};
