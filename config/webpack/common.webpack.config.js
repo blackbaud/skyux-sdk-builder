927ee7bc7f03296485024afdf46ac3416430bee3
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-05-15 14:08:28 -0400

    [rc] Improve AoT bundle size

--- config/webpack/common.webpack.config.js
+++ config/webpack/common.webpack.config.js
@@ -74,9 +74,13 @@ function getWebpackConfig(skyPagesConfig, argv = {}) {
     // The `ngDevMode` property is defined by Angular CLI during a production build.
     // We need to set it to 'false' to get the build to pass.
     // See: https://github.com/angular/angular/issues/31595#issuecomment-519083266
+    // Setting these values to `false` also addresses some payload size issues.
+    // See: https://angular.io/guide/ivy-compatibility#payload-size-debugging
     new webpack.DefinePlugin({
       'skyPagesConfig': JSON.stringify(skyPagesConfig),
-      'ngDevMode': false
+      'ngDevMode': false,
+      'ngI18nClosureMode': false,
+      'ngJitMode': false
     }),
 
     new LoaderOptionsPlugin({
