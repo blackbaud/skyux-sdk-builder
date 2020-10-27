d0f95d11427a934a5215088f32ae28a8899d9b03
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-10-27 16:15:21 -0400

    Babel runtime

--- config/webpack/serve.webpack.config.js
+++ config/webpack/serve.webpack.config.js
@@ -64,7 +64,9 @@ function getWebpackConfig(argv, skyPagesConfig) {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env']
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime'],
+              regenerator: true
             }
           }
         }
