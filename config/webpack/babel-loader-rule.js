22aa4e3a6278d8cce6289ee6d0cfa6707719d906
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-10-29 10:18:36 -0400

    Add babel util

---
+++ config/webpack/babel-loader-rule.js
@@ -0,0 +1,35 @@
+/**
+ * Returns a Webpack rule that converts the provided package dependencies to ES5.
+ * @param {*} nonES5Dependencies An array of package dependency names that are not ES5 compatible.
+ */
+function getES5Rule(nonES5Dependencies = []) {
+
+  // The following packages are provided by Webpack Dev Server and are not compatible
+  // with our ES5 target.
+  // See: https://github.com/blackbaud/skyux-sdk-builder/issues/220
+  const webServerPackages = [
+    'strip-ansi',
+    'ansi-regex',
+  ];
+
+  const dependencies = [
+    ...webServerPackages,
+    ...nonES5Dependencies
+  ];
+
+  return {
+    test: new RegExp(`(\\|\/)(${dependencies.join('|')})(\\|\/)`),
+    include: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-transform-runtime']
+      }
+    }
+  };
+}
+
+module.exports = {
+  getES5Rule
+};
