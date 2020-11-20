c9e2ecf631a223e70796f73d62b51333cceda879
Steve Brush <steve.brush@blackbaud.com>
2020-11-20 09:26:16 -0500

    Implement GitHub actions (#332)
    * Implement GitHub actions
    
    * Fix tests

--- e2e/skyux-test.e2e-spec.js
+++ e2e/skyux-test.e2e-spec.js
@@ -5,7 +5,7 @@ const common = require('./shared/common');
 
 describe('skyux test', () => {
   it('should successfully run unit tests', (done) => {
-    common.exec(`node`, [common.cliPath, `test`], common.cwdOpts)
+    common.exec(`node`, [common.cliPath, `test`, '--headless'], common.cwdOpts)
       .then(exit => {
         expect(exit).toEqual(0);
         done();
